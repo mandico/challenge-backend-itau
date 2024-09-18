0a6fc19561b36417848f57412329251144cf40cf
Luiz Mandico <luizmandico@gmail.com>
2024-09-18 11:14:12 -0300

    IaC - Azure

---
+++ iac/azure/main.tf
@@ -0,0 +1,39 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+resource "azurerm_resource_group" "aks" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks.location
+  resource_group_name = azurerm_resource_group.aks.name
+  dns_prefix          = "aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
---
+++ iac/azure/variables.tf
@@ -0,0 +1,40 @@
+variable "client_id" {
+  description = "The Client ID of the Azure Service Principal"
+}
+
+variable "client_secret" {
+  description = "The Client Secret of the Azure Service Principal"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID in Azure"
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID in Azure"
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  default     = "azr-rg-challenge-n"
+}
+
+variable "location" {
+  description = "The Azure region to create resources in"
+  default     = "East US"
+}
+
+variable "aks_cluster_name" {
+  description = "The name of the AKS cluster"
+  default     = "azr-aks-challenge-n"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the AKS cluster"
+  default     = 3
+}
+
+variable "node_size" {
+  description = "The size of the nodes in the AKS cluster"
+  default     = "Standard_DS2_v2"
+}
