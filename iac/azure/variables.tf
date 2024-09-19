3481d1d64563ef64fc0e528665424cf824747f62
Luiz Mandico <luizmandico@gmail.com>
2024-09-18 22:56:55 -0300

    Change Tier VM

--- iac/azure/variables.tf
+++ iac/azure/variables.tf
@@ -36,5 +36,5 @@ variable "node_count" {
 
 variable "node_size" {
   description = "The size of the nodes in the AKS cluster"
-  default     = "Standard_DS2_v2"
+  default     = "Standard_D2as_v5"
 }
